4699878d82ee0854134af1615b095dff60ceafda
Eli Kovalev <elisey.k01@gmail.com>
2023-07-04 14:02:42 -0700

    modified:   script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function loadHtml(id, filename) {
         xhr.onreadystatechange = function(){
             if (this.readyState == 4 && this.status == 200) {
                 element.innerHTML = this.responseText;
-            } else element.innerHTML = '<h1> Errrrrrrrrror :[</h1>';
+            }
         };
         xhr.open("GET", `pages/${file}`, true)
         xhr.send();
